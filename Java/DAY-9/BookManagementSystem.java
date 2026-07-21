fd0ea48e3c2060d80e9c8dbb4132500d8dac7b2f
Tamizhazhagan-SK <tamizhazhagan.s@bmwtechworks.in>
2026-07-21 16:43:15 +0530

    chore: DAY-9 session over

--- Java/DAY-9/BookManagementSystem.java
+++ Java/DAY-9/BookManagementSystem.java
@@ -7,19 +7,10 @@ public class BookManagementSystem {
 
         try {
             Scanner sc = new Scanner(System.in);
-
-            Connection con = DriverManager.getConnection(
-                    "jdbc:h2:mem:test", "ben", "stokes");
-
+            Connection con = DriverManager.getConnection("jdbc:h2:mem:test", "ben", "stokes");
             Statement stmt = con.createStatement();
 
-            stmt.execute(
-                    "CREATE TABLE Book(" +
-                    "bookId INT PRIMARY KEY," +
-                    "title VARCHAR(100)," +
-                    "copiesAvailable INT," +
-                    "publicationYear INT)"
-            );
+            stmt.execute("CREATE TABLE Book(" + "bookId INT PRIMARY KEY," + "title VARCHAR(100)," + "copiesAvailable INT," + "publicationYear INT)");
 
             processMenu(con, sc);
 
@@ -35,9 +26,7 @@ public class BookManagementSystem {
     static void processMenu(Connection con, Scanner sc) {
 
         int choice;
-
         do {
-
             System.out.println("\n1. Insert Book");
             System.out.println("2. Display Books Sorted by Publication Year and Title");
             System.out.println("3. Display Books by Title Pattern");
@@ -45,7 +34,6 @@ public class BookManagementSystem {
             System.out.println("5. Update Copies Available");
             System.out.println("6. Exit");
             System.out.print("Enter your choice: ");
-
             choice = sc.nextInt();
 
             switch (choice) {
@@ -54,27 +42,22 @@ public class BookManagementSystem {
                 case 1:
                     insertBook(con, sc);
                     break;
-
                 // DISPLAY BOOKS SORTED
                 case 2:
                     displayBooksSorted(con);
                     break;
-
                 // DISPLAY BOOKS BY TITLE PATTERN
                 case 3:
                     displayBooksByTitlePattern(con, sc);
                     break;
-
                 // DISPLAY ALL BOOKS
                 case 4:
                     displayAllBooks(con);
                     break;
-
                 // UPDATE COPIES AVAILABLE
                 case 5:
                     updateCopies(con, sc);
                     break;
-
                 // EXIT APPLICATION
                 case 6:
                     System.out.println("\nApplication terminated");
@@ -90,12 +73,9 @@ public class BookManagementSystem {
     static void insertBook(Connection con, Scanner sc) {
 
         try {
-
             int bookId = sc.nextInt();
             sc.nextLine();
-
             String title = sc.nextLine();
-
             int copies = sc.nextInt();
             int year = sc.nextInt();
 
@@ -106,12 +86,10 @@ public class BookManagementSystem {
             ps.setString(2, title);
             ps.setInt(3, copies);
             ps.setInt(4, year);
-
             ps.executeUpdate();
 
             System.out.println("\nBook inserted successfully");
             displayAllBooks(con);
-
             ps.close();
 
         } catch (Exception e) {
@@ -122,26 +100,21 @@ public class BookManagementSystem {
     static void displayBooksSorted(Connection con) {
 
         try {
-
             Statement st = con.createStatement();
 
-            ResultSet rs = st.executeQuery(
-                    "SELECT * FROM Book ORDER BY publicationYear ASC, title DESC");
+            ResultSet rs = st.executeQuery("SELECT * FROM Book ORDER BY publicationYear ASC, title DESC");
 
             System.out.println("\nBooks sorted by publication year and title:");
 
             while (rs.next()) {
-
                 System.out.println(
                         "Book Id = " + rs.getInt("bookId")
                         + ", Title = " + rs.getString("title")
                         + ", Copies = " + rs.getInt("copiesAvailable")
                         + ", Year of publication = " + rs.getInt("publicationYear"));
             }
-
             rs.close();
             st.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -150,12 +123,9 @@ public class BookManagementSystem {
     static void displayBooksByTitlePattern(Connection con, Scanner sc) {
 
         try {
-
             String pattern = sc.next();
-
             PreparedStatement ps =
-                    con.prepareStatement(
-                            "SELECT * FROM Book WHERE title LIKE ?");
+                    con.prepareStatement("SELECT * FROM Book WHERE title LIKE ?");
 
             ps.setString(1, pattern + "%");
 
@@ -164,17 +134,14 @@ public class BookManagementSystem {
             System.out.println("\nBooks with title starting with " + pattern + ":");
 
             while (rs.next()) {
-
                 System.out.println(
                         "Book Id = " + rs.getInt("bookId")
                         + ", Title = " + rs.getString("title")
                         + ", Copies = " + rs.getInt("copiesAvailable")
                         + ", Year of publication = " + rs.getInt("publicationYear"));
             }
-
             rs.close();
             ps.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -183,7 +150,6 @@ public class BookManagementSystem {
     static void displayAllBooks(Connection con) {
 
         try {
-
             Statement st = con.createStatement();
 
             ResultSet rs = st.executeQuery("SELECT * FROM Book");
@@ -191,17 +157,14 @@ public class BookManagementSystem {
             System.out.println("All Books:");
 
             while (rs.next()) {
-
                 System.out.println(
                         "Book Id = " + rs.getInt("bookId")
                         + ", Title = " + rs.getString("title")
                         + ", Copies = " + rs.getInt("copiesAvailable")
                         + ", Year of publication = " + rs.getInt("publicationYear"));
             }
-
             rs.close();
             st.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -210,22 +173,17 @@ public class BookManagementSystem {
     static void updateCopies(Connection con, Scanner sc) {
 
         try {
-
             int bookId = sc.nextInt();
             int newCopies = sc.nextInt();
 
             PreparedStatement ps =
-                    con.prepareStatement(
-                            "UPDATE Book SET copiesAvailable=? WHERE bookId=?");
+                    con.prepareStatement("UPDATE Book SET copiesAvailable=? WHERE bookId=?");
 
             ps.setInt(1, newCopies);
             ps.setInt(2, bookId);
-
             ps.executeUpdate();
-
             System.out.println("\nBook updated successfully");
             displayAllBooks(con);
-
             ps.close();
 
         } catch (Exception e) {
